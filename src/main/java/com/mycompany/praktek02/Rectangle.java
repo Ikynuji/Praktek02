98b24f26d06c16f0fc530fc65906b9b946e32f99
Ikynuji <rizky.new.appp@gmail.com>
2023-07-18 14:47:36 +0700

    Membuat Attribute dan Behavior

--- src/main/java/com/mycompany/praktek02/Rectangle.java
+++ src/main/java/com/mycompany/praktek02/Rectangle.java
@@ -9,5 +9,13 @@ package com.mycompany.praktek02;
  * @author User
  */
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("=================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar :   "+lebar);
+        System.out.println("=================");
+    }
 }
